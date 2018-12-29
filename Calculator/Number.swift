00986364dd6fa45e13abacb10beae9fc6c4abeee
Alexander Shenshin <shenshin@me.com>
2018-12-30 02:23:29 +0300

    Checking the input to provide a valid data for calculations.

---
+++ Calculator/Number.swift
@@ -0,0 +1,17 @@
+//
+//  Number.swift
+//  Calculator
+//
+//  Created by Алесь Шеншин on 29/12/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Number {
+    var value: Double = 0
+    
+    class func isValid(_ input: String)->Bool {
+        return true
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,22 +10,76 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private var isFinishedTypingNumber : Bool = true
+    
+ 
+    //var number: Number = Number()
+//    var titleString : String = "0"
+//    var number : Int {
+//        get {
+//            if let number = Int(titleString) {
+//                return number
+//            } else {
+//                return 0
+//            }
+//        }
+//    }
+    
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        displayLabel.text = titleString
+//    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        
+        isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Can not convert display label to a double")
+        }
+        print(number)
+        if let calcMethod = sender.currentTitle {
+            switch calcMethod {
+            case "+/-":
+                displayLabel.text = String(number * -1)
+            case "AC":
+                displayLabel.text = "0"
+            case "%":
+                displayLabel.text = String(number / 100)
+            default:
+                return
+                //displayLabel.text = String(number)
+            }
+        }
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a number is entered into the keypad
-    
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                if numValue == "." {
+                    displayLabel.text = "0."
+                } else {
+                    displayLabel.text = numValue
+                }
+                isFinishedTypingNumber = false
+            } else {
+                
+                if numValue == "." && displayLabel.text!.contains("."){
+                    return
+                }
+                guard Double(displayLabel.text!) != nil else {
+                    fatalError("Cannot conver display label to a double")
+                }
+                displayLabel.text! += numValue
+            }
+        }
     }
-
 }
 
